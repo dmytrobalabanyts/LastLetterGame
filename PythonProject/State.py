845db9d6004a2809034b5982617f0623b481bb18
dmytrobalabanyts <51229710+dmytrobalabanyts@users.noreply.github.com>
2019-06-04 14:26:42 +0300

    Some improvements

--- PythonProject/State.py
+++ PythonProject/State.py
@@ -4,18 +4,26 @@ import Utils
 class State(object):
     """description of class"""
     def __init__(self, dictionary):
-        self.dictionary = dictionary
+        self.__dictionary = dictionary
 
-    def __add_to_used_words(self, __last_word):
-        pass
+    def __add_to_used_words(self, last_word):
+        self.__used_words.add(__last_word)
 
     def notify(self, string):
         notification = json.loads(string)
         if notification['command'] == 'validate':
             return '{"answer": "validated"}'
         elif notification['command'] == 'used':
-            return '{"answer": "null"}'
+            if self.__word_validator(notification['data']):
+                return '{"answer": "validated"}'
+            else:
+                return '{"answer": "invalidated"}'
 
-    dictionary = None
+    def __word_validator(self, word):
+        word_is_correct = True
+        if word in self.__used_words:
+            word_is_correct = False
+        return word_is_correct
 
-
+    __dictionary = None
+    __used_words = set()
